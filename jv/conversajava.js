cd688a7e357fc21c0023dc8c740009ba193ce18e
Dnimf <168024451+Dnimf@users.noreply.github.com>
2024-11-06 08:19:11 -0300

    iii

--- jv/conversajava.js
+++ jv/conversajava.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    popup1=document.querySelector('.tabela1')
-    
-    popup1.addEventListener('click', function(){
-        popup1.style.backgroundColor='black'
+    popup1=document.querySelectorAll('img')
+    popup2=popup1[1]
+    popup2.addEventListener('click', function(){
+        popup2.style.backgroundColor='black'
+        console.log('a')
     })
 })
